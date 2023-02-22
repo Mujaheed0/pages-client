dda31e7f50b3fc1f20ebd5c3ee9d54e1b64d83cf
Mujaheed <mujaheedhussain07@gmail.com>
2023-02-22 07:48:29 +0530

    demo preview

--- src/store/features/Auth/services.js
+++ src/store/features/Auth/services.js
@@ -1,4 +1,5 @@
 import { toast } from "react-hot-toast";
+import { backendUrl } from "../../../utils/backendUrl";
 
 export const emailPasswordSignUp = async (thunkObj) => {
   const loading = toast.loading("Creating Account");
